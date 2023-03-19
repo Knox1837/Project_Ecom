47ef5cf341a0a372e1835fd888a4ef43c6af2047
Knox <kaustuvm1837@gmail.com>
2023-03-19 08:17:26 +0545

    Update forms.py

--- Project_ecom/Ecom_app/forms.py
+++ Project_ecom/Ecom_app/forms.py
@@ -4,5 +4,5 @@ from .models import Product
 class ProductAddForm(forms.ModelForm):
     class Meta:
         #fields="__all__" for all fields
-        fields=("title", "desc", "category", "price", "image", "quality", "discount", "cash_on_delivery"), #for selective fields      
+        fields=("title", "desc", "category", "price", "image", "quantity", "discount", "cash_on_delivery"), #for selective fields      
         model=Product
